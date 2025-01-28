f3a55de0f09079100a7525b49b646d7075ce5895
mohitsharma012 <stayer.mohit@gmail.com>
2025-01-28 23:27:41 +0530

    changes

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,7 +1,12 @@
 import * as admin from 'firebase-admin';
 import { getApps } from 'firebase-admin/app'; // You don't need to re-import initializeApp or cert
 
-import serviceAccountKey from '@/serviceAccountKey.json';
+
+const serviceAccountKey = {
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // Handle newlines
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+  };
 
 const firebaseAdminConnection = (): void => {
   // Check if Firebase Admin is already initialized
